e76fbb19620490060031d8f8444fba63f9e4dded
dev-abir <gangulyabir777@gmail.com>
2022-02-16 15:35:18 +0530

    minor changes

--- chitchat-server/chitchat_server_project/chitchat_server_project/asgi.py
+++ chitchat-server/chitchat_server_project/chitchat_server_project/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chitchat_server_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chitchat_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chitchat_server_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
